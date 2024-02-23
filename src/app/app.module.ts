f4816f63f43b34f495ad1356299c317925adf78c
Mitul <mitulgondaliya27@gmail.com>
2024-02-23 10:48:26 +0530

    complete this task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import { AddStudentsComponent } from './components/add-students/add-students.component';
 import { EditStudentsComponent } from './components/edit-students/edit-students.component';
 import { ListStudentsComponent } from './components/list-students/list-students.component';
-import { LoginStudentsComponent } from './components/login-students/login-students.component';
-import { RegistrationStudentsComponent } from './components/registration-students/registration-students.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgToastModule } from 'ng-angular-popup';
@@ -18,10 +16,7 @@ import { NgToastModule } from 'ng-angular-popup';
     AppComponent,
     AddStudentsComponent,
     EditStudentsComponent,
-    ListStudentsComponent,
-    LoginStudentsComponent,
-    RegistrationStudentsComponent
-   
+    ListStudentsComponent   
   ],
   imports: [
     BrowserModule,
